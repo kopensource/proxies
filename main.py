852f5697299abd3f26db251b048abcc7d1d96941
Kristof Kovacs <kristofk@kristofkmbpm1m.local>
2025-01-15 15:55:54 +0200

    update

--- main.py
+++ main.py
@@ -6,15 +6,15 @@ from src import DBUrls, ProxyFetcher
 URLS = DBUrls(
     https = [
         'https://raw.githubusercontent.com/vakhov/fresh-proxy-list/refs/heads/master/https.txt', # Good, but not many
-        # 'https://raw.githubusercontent.com/officialputuid/KangProxy/KangProxy/https/https.txt', # Good
-        # 'https://raw.githubusercontent.com/ErcinDedeoglu/proxies/refs/heads/main/proxies/https.txt', # Good
-        # 'https://raw.githubusercontent.com/Zaeem20/FREE_PROXIES_LIST/refs/heads/master/https.txt', # Ok
+        'https://raw.githubusercontent.com/officialputuid/KangProxy/KangProxy/https/https.txt', # Good
+        'https://raw.githubusercontent.com/ErcinDedeoglu/proxies/refs/heads/main/proxies/https.txt', # Good
+        'https://raw.githubusercontent.com/Zaeem20/FREE_PROXIES_LIST/refs/heads/master/https.txt', # Ok
     ],
     http = [
         'https://raw.githubusercontent.com/MuRongPIG/Proxy-Master/refs/heads/main/http_checked.txt', # Good, but not many
-        # 'https://raw.githubusercontent.com/monosans/proxy-list/refs/heads/main/proxies_anonymous/http.txt', # Good
-        # 'https://raw.githubusercontent.com/sunny9577/proxy-scraper/refs/heads/master/generated/http_proxies.txt', # Ok
-        # 'https://raw.githubusercontent.com/TheSpeedX/PROXY-List/refs/heads/master/http.txt', # Ok
+        'https://raw.githubusercontent.com/monosans/proxy-list/refs/heads/main/proxies_anonymous/http.txt', # Good
+        'https://raw.githubusercontent.com/sunny9577/proxy-scraper/refs/heads/master/generated/http_proxies.txt', # Ok
+        'https://raw.githubusercontent.com/TheSpeedX/PROXY-List/refs/heads/master/http.txt', # Ok
     ]
 )
 MAX_WORKERS = 25
